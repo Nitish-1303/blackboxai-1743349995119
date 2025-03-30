d3e1f90046a8c7ba54cc83b525cc37643d71fdd4
Nitish-1303 <141549775+Nitish-1303@users.noreply.github.com>
2025-03-30 21:23:40 +0530

    Add babel.config.js 1743350007954

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+export default {
+  presets: [
+    ['@babel/preset-env', {targets: {node: 'current'}}],
+    '@babel/preset-react'
+  ],
+  plugins: [
+    '@babel/plugin-transform-modules-commonjs'
+  ]
+};
